1623d3b0128e563b51a9a3602e695b66fe82fbb6
Loreto Barroso <loreto.barroso@students.dominican.edu>
2020-10-01 14:21:54 -0700

    Users can edit and update events

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Initialize express
 const express = require('express')
+const methodOverride = require('method-override')
+
 const app = express()
 
 // INITIALIZE BODY-PARSER AND ADD IT TO APP
@@ -15,6 +17,9 @@ const exphbs = require('express-handlebars');
 
 const Handlebars = require('handlebars')
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+
+// override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'))
 
 // Use "main" as our default layout
 app.engine('handlebars', exphbs({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
@@ -67,6 +72,29 @@ app.get('/events/:id', (req, res) => {
     console.log(err.message);
   })
 })
+
+// EDIT
+app.get('/events/:id/edit', (req, res) => {
+  models.Event.findByPk(req.params.id).then((event) => {
+    res.render('events-edit', { event: event });
+  }).catch((err) => {
+    console.log(err.message);
+  })
+});
+
+// UPDATE
+app.put('/events/:id', (req, res) => {
+  models.Event.findByPk(req.params.id).then(event => {
+    event.update(req.body).then(event => {
+      res.redirect(`/events/${req.params.id}`);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+
 
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
