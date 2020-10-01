f3382473d04c6e5d2655ec3e0e808f1be6e1a48f
Loreto Barroso <loreto.barroso@students.dominican.edu>
2020-10-01 14:42:07 -0700

    proper separation of concerns

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const app = express()
 const bodyParser = require('body-parser');
 
 const models = require('./db/models');
+require('./controllers/events')(app, models);
+
 
 // The following line must appear AFTER const app = express() and before your routes!
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,83 +29,6 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main', handlebars: allowInsecu
 // Use handlebars to render
 app.set('view engine', 'handlebars');
 
-// Render the "home" layout for the main page and send the following msg
-
-
-// OUR MOCK ARRAY OF PROJECTS
-//var events = [
-//  { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
-//  { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
-//  { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
-//]
-
-// INDEX
-app.get('/', (req, res) => {
-  models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
-console.log(events);
-    res.render('events-index', { events: events });
-  })
-})
-
-
-
-// NEW
-app.get('/events/new', (req, res) => {
-  res.render('events-new', {});
-})
-
-// CREATE
-app.post('/events', (req, res) => {
-  models.Event.create(req.body).then(event => {
-    res.redirect(`/events/${event.id}`)
-  }).catch((err) => {
-    console.log(err)
-  });
-})
-
-// SHOW
-app.get('/events/:id', (req, res) => {
-  // Search for the event by its id that was passed in via req.params
-  models.Event.findByPk(req.params.id).then((event) => {
-    // If the id is for a valid event, show it
-    res.render('events-show', { event: event })
-  }).catch((err) => {
-    // if they id was for an event not in our db, log an error
-    console.log(err.message);
-  })
-})
-
-// EDIT
-app.get('/events/:id/edit', (req, res) => {
-  models.Event.findByPk(req.params.id).then((event) => {
-    res.render('events-edit', { event: event });
-  }).catch((err) => {
-    console.log(err.message);
-  })
-});
-
-// UPDATE
-app.put('/events/:id', (req, res) => {
-  models.Event.findByPk(req.params.id).then(event => {
-    event.update(req.body).then(event => {
-      res.redirect(`/events/${req.params.id}`);
-    }).catch((err) => {
-      console.log(err);
-    });
-  }).catch((err) => {
-    console.log(err);
-  });
-});
-
-// DELETE
-app.delete('/events/:id', (req, res) => {
-  models.Event.findByPk(req.params.id).then(event => {
-    event.destroy();
-    res.redirect(`/`);
-  }).catch((err) => {
-    console.log(err);
-  });
-})
 
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
